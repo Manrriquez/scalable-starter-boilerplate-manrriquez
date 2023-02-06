784eb04f23628b728d49fc7d9d58dd2ce9c11767
Manrriquez <manrriquez.contato@gmail.com>
2023-02-06 13:06:06 -0300

    fix: resolve bootstrap sass convert to css using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 var rename = require('gulp-rename');
-
+var sourcemaps = require('gulp-sourcemaps');
 
 /*
  * Variables
  */
 // Sass Source
 var scssFiles = './assets/sass/main.scss';
+
+// Bootstrap Sass Source
+var scssFilesBootstrap = './assets/libs/bootstrap/bootstrap.scss';
+
+// Bootstrap Sass Destination
+var cssBootstrapDest = './assets/libs/bootstrap/css/';
+
 
 // CSS destination
 var cssDest = './assets/css';
@@ -40,10 +47,16 @@ gulp.task('sassprod', function() {
     .pipe(gulp.dest(cssDest));
 });
 
-// Task 'watch' - Run with command 'gulp watch'
-gulp.task('watch', function() {
-  gulp.watch(scssFiles,  gulp.series('sassdev', 'sassprod'));
+// Task 'sassprod-bootstrap' - Run with command 'gulp sassprodbootstrap'
+gulp.task('sassprodbootstrap', function() {
+  return gulp.src(scssFilesBootstrap,  { allowEmpty: true })
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassProdOptions).on('error', sass.logError))
+    .pipe(rename('bootstrap.min.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(cssBootstrapDest));
 });
 
+
 // Default task - Run with command 'gulp'
-gulp.task('default', gulp.series('sassdev', 'sassprod', 'watch'));
+gulp.task('default', gulp.series('sassdev', 'sassprod', 'sassprodbootstrap'));
